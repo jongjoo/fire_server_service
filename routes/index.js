0356e86b85de9f53549f69d67c03024be599b108
root <root@fire-han.ga>
2017-10-01 00:44:24 +0900

    web_test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+var mysql = require('mysql');
+var msg = require('../message.js');
+
+var db_config = require('../config/db_config.json');
+
+var pool = mysql.createPool(db_config);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/te', function(res, res){
+
+
+});
+
+router.get('/insert', function (request, response) {
+  fs.readFile('/views/insert.html', 'utf8', function (error, data) {
+    response.send(data);
+  });
+});
+
+router.post('/insert', function (request, response) {
+  var body = request.body;
+  client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
+      body.name, body.modelnumber, body.series
+  ], function () {
+    response.redirect('/');
+  });
 });
 
 module.exports = router;
