7e2264019d2cdf1cefb3f534a60ff5eeb9c849c9
root <root@fire-han.ga>
2017-10-16 02:35:09 +0900

    fcm

--- routes/index.js
+++ routes/index.js
@@ -8,29 +8,45 @@ var db_config = require('../config/db_config.json');
 
 var pool = mysql.createPool(db_config);
 
+
+pool.getConnection(function(err,con){
+
+	con.query('select * from fire', function(err,result){
+	});
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	pool.getConnection(function(err,con){
+        	con.query('select * from test', function(err,result){
+	
+  			res.render('index', { 
+				data: result,
+				title: 'Express' });
+			con.release();
+	      	});
+	});
+  //res.render('index', { title: 'Express' });
+
+});
+/*
+router.post('/write', function(req, res, next){
+	pool.getConnection(function(err,con){
+	  if (err) {
+            console.log("getConnection Error" + err);
+            res.sendStatus(500);
+        } else {
+		  console.log("호스팅완료");
+
+		var sqlForInsert = "insert into test (test) values(?)";
+		con.query(sqlForInsert, req.body.test, function(err, result){
+			if(err)
+				console.log('insert error: ', err.message);
+			else
+				res.redirect('/');
+		});
+	}
+	});
 });
 
-router.post('/te', function(res, res){
-
-
-});
-
-router.get('/insert', function (request, response) {
-  fs.readFile('/views/insert.html', 'utf8', function (error, data) {
-    response.send(data);
-  });
-});
-
-router.post('/insert', function (request, response) {
-  var body = request.body;
-  client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
-      body.name, body.modelnumber, body.series
-  ], function () {
-    response.redirect('/');
-  });
-});
-
+*/
 module.exports = router;
